6971632cfd6b19d4cfb6dd62be4b7ba82408fb61
HenryZhuHR <296506195@qq.com>
2024-04-23 13:40:06 +0800

    📝 docs: 添加文档及action部署

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,39 @@
+import { viteBundler } from '@vuepress/bundler-vite'
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+import { copyCodePlugin } from '@vuepress/plugin-copy-code'
+import { backToTopPlugin } from '@vuepress/plugin-back-to-top'
+import { docsearchPlugin } from '@vuepress/plugin-docsearch'
+import { externalLinkIconPlugin } from '@vuepress/plugin-external-link-icon'
+import { gitPlugin } from '@vuepress/plugin-git'
+
+export default defineUserConfig({
+    base: '/detect-segment-anything/',
+    title: '开放世界的检测与分割',
+    description: '开放世界的检测与分割项目文档',
+    bundler: viteBundler(),
+    theme: defaultTheme(),
+    plugins: [
+        // https://ecosystem.vuejs.press/zh/plugins/copy-code.html
+        copyCodePlugin({
+            showInMobile: true,
+        }),
+        // https://ecosystem.vuejs.press/zh/plugins/back-to-top.html
+        backToTopPlugin(),
+        // https://ecosystem.vuejs.press/zh/plugins/docsearch.html
+        docsearchPlugin({
+            locales: {
+                '/': {
+                    placeholder: '搜索文档',
+                    translations: {
+                        button: {
+                            buttonText: '搜索文档',
+                        },
+                    },
+                },
+            }
+        }),
+        externalLinkIconPlugin({}),
+        gitPlugin({}),
+    ],
+})
